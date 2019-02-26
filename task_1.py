22da9e12f05ce4ba16e6f98379f703e345909493
Florian Wintel <florianwintel@googlemail.com>
2019-02-26 17:48:25 +0100

    Upgraded task 1. Added model for task 2. Updated plotters.

--- task_1.py
+++ task_1.py
@@ -11,6 +11,9 @@ import torch
 from torch import nn
 from dataloaders import load_cifar10
 from utils import to_cuda, compute_loss_and_accuracy
+
+#print(torch.cuda.current_device())
+print(torch.cuda.is_available())
  
 #np.random.seed(7)
 
@@ -23,8 +26,55 @@ class Model_task1(nn.Module):
                           kernel_size=5,
                           stride=1,
                           padding=2),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2)
+                )
+        self.layer2 = nn.Sequential(
+                nn.Conv2d(in_channels=32,
+                          out_channels=64,
+                          kernel_size=5,
+                          stride=1,
+                          padding=2),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2)
+                )
+        self.layer3 = nn.Sequential(
+                nn.Conv2d(in_channels=64,
+                          out_channels=128,
+                          kernel_size=5,
+                          stride=1,
+                          padding=2),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2)
+                )
+        self.num_output_features = 128*4*4
+        self.classifier = nn.Sequential(
+                nn.Linear(self.num_output_features,64),
+                nn.Linear(64,num_classes),
+                #nn.Softmax()
+                )
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = x.view(-1, self.num_output_features)
+        x = self.classifier(x)
+        return x
+
+
+class Model_task2_a(nn.Module):
+    def __init__(self, image_channels=3, num_classes=10, batch_size=32):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+                nn.Conv2d(in_channels=image_channels,
+                          out_channels=32,
+                          kernel_size=5,
+                          stride=1,
+                          padding=2),
+                nn.BatchNorm2d(num_features = 32),
                 nn.MaxPool2d(kernel_size=2, stride=2),
-                nn.ReLU()
+                nn.ReLU(),
+                nn.Dropout(p=0)
                 )
         self.layer2 = nn.Sequential(
                 nn.Conv2d(in_channels=32,
@@ -33,7 +83,8 @@ class Model_task1(nn.Module):
                           stride=1,
                           padding=2),
                 nn.MaxPool2d(kernel_size=2, stride=2),
-                nn.ReLU()
+                nn.ReLU(),
+                nn.Dropout(p=0)
                 )
         self.layer3 = nn.Sequential(
                 nn.Conv2d(in_channels=64,
@@ -42,7 +93,8 @@ class Model_task1(nn.Module):
                           stride=1,
                           padding=2),
                 nn.MaxPool2d(kernel_size=2, stride=2),
-                nn.ReLU()
+                nn.ReLU(),
+                nn.Dropout(p=0)
                 )
         self.num_output_features = 128*4*4
         self.classifier = nn.Sequential(
@@ -67,8 +119,10 @@ class Trainer:
         """
         # Define hyperparameters
         self.epochs = 30
-        self.batch_size = 64
-        self.learning_rate = 1e-1
+        self.batch_size = 32
+        self.learning_rate = 5e-2
+        self.weight_decay = 0
+        self.dropout = 0
         self.early_stop_count = 3
 
         # Architecture
@@ -77,12 +131,14 @@ class Trainer:
         self.loss_criterion = nn.CrossEntropyLoss()
         # Initialize the mode
         self.model = Model_task1(image_channels=3, num_classes=10)
+        #self.model = Model_task1(image_channels=3, num_classes=10, batch_size=self.batch_size)
         # Transfer model to GPU VRAM, if possible.
         self.model = to_cuda(self.model)
 
         # Define our optimizer. SGD = Stochastich Gradient Descent
         self.optimizer = torch.optim.SGD(self.model.parameters(),
-                                         self.learning_rate)
+                                         lr = self.learning_rate,
+                                         weight_decay = self.weight_decay)
 
         # Load our dataset
         self.dataloader_train, self.dataloader_val, self.dataloader_test = load_cifar10(self.batch_size)
@@ -108,21 +164,21 @@ class Trainer:
         train_loss, train_acc = compute_loss_and_accuracy(
             self.dataloader_train, self.model, self.loss_criterion
         )
-        self.TRAIN_ACC.append(train_acc)
+        self.TRAIN_ACC.append(train_acc*100)
         self.TRAIN_LOSS.append(train_loss)
 
         # Compute for validation set
         validation_loss, validation_acc = compute_loss_and_accuracy(
             self.dataloader_val, self.model, self.loss_criterion
         )
-        self.VALIDATION_ACC.append(validation_acc)
+        self.VALIDATION_ACC.append(validation_acc*100)
         self.VALIDATION_LOSS.append(validation_loss)
-        #print("Current validation loss:", validation_loss, " Accuracy:", validation_acc)
+        print("Current validation loss:", validation_loss, " Accuracy:", validation_acc)
         # Compute for testing set
         test_loss, test_acc = compute_loss_and_accuracy(
             self.dataloader_test, self.model, self.loss_criterion
         )
-        self.TEST_ACC.append(test_acc)
+        self.TEST_ACC.append(test_acc*100)
         self.TEST_LOSS.append(test_loss)
 
         self.model.train()
@@ -162,7 +218,7 @@ class Trainer:
                 # Perform the forward pass
                 predictions = self.model(X_batch)
                 # Compute the cross entropy loss for the batch
-                loss = self.loss_criterion(predictions, Y_batch)
+                loss = self.loss_criterion(predictions, Y_batch)    
 
                 # Backpropagation
                 loss.backward()
@@ -184,25 +240,42 @@ if __name__ == "__main__":
     trainer = Trainer()
     trainer.train()
 
+
+def plot_metrics():
+    """
+    Plots the metrics and prints the final results.
+    """
     os.makedirs("plots", exist_ok=True)
-    # Save plots and show them
-    plt.figure(figsize=(12, 8))
-    plt.title("Cross Entropy Loss")
-    plt.plot(trainer.VALIDATION_LOSS, label="Validation loss")
-    plt.plot(trainer.TRAIN_LOSS, label="Training loss")
-    plt.plot(trainer.TEST_LOSS, label="Testing Loss")
-    plt.legend()
-    plt.savefig(os.path.join("plots", "final_loss.png"))
+    
+    fig1, ax1 = plt.subplots(1,1,figsize=(6,3.5),dpi=200)
+    test_metric, = ax1.plot(trainer.TEST_LOSS, label="Testing Loss", color="red")
+    validation_metric, = ax1.plot(trainer.VALIDATION_LOSS, label="Validation Loss", color="orange")
+    training_metric, = ax1.plot(trainer.TRAIN_LOSS, label="Training Loss", color="blue")
+    ax1.legend(handles=[training_metric,validation_metric,test_metric])
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Cross Entropy Loss")
     #plt.show()
 
-    plt.figure(figsize=(12, 8))
-    plt.title("Accuracy")
-    plt.plot(trainer.VALIDATION_ACC, label="Validation Accuracy")
-    plt.plot(trainer.TRAIN_ACC, label="Training Accuracy")
-    plt.plot(trainer.TEST_ACC, label="Testing Accuracy")
-    plt.legend()
-    plt.savefig(os.path.join("plots", "final_accuracy.png"))
-    #plt.show()
+    fig1.savefig(os.path.join("plots", "A3_T2_a_multiclass_crossentropy.eps"))
 
+    fig2, ax1 = plt.subplots(1,1,figsize=(6,3.5),dpi=200)
+    test_metric, = ax1.plot(trainer.TEST_ACC, label="Testing Accuracy", color="red")
+    validation_metric, = ax1.plot(trainer.VALIDATION_ACC, label="Validation Accuracy", color="orange")
+    train_metric, = ax1.plot(trainer.TRAIN_ACC, label="Training Accuracy", color="blue")
+    ax1.legend(handles=[training_metric,validation_metric,test_metric])
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Accuracy (%)")
+    #plt.show()
+    
+    fig2.savefig(os.path.join("plots", "A3_T2_a_accuracy.eps"))
+    
+    print([param.nelement() for param in trainer.model.parameters()])
+    print("Connections: " + str(sum([param.nelement() for param in trainer.model.parameters()][::2])))
+    print("Biases: " + str(sum([param.nelement() for param in trainer.model.parameters()][1::2])))
+    print("Trainable Parameters: " + str(sum([param.nelement() for param in trainer.model.parameters()])))
+
+    print("Final training accuracy:", trainer.TRAIN_ACC[-trainer.early_stop_count])
     print("Final test accuracy:", trainer.TEST_ACC[-trainer.early_stop_count])
     print("Final validation accuracy:", trainer.VALIDATION_ACC[-trainer.early_stop_count])
+
+plot_metrics()
